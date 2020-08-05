b0e369e7cc0b8cf26e3614d60109ada0f6c59e02
Chun-Hung Jimmy Yeh <jimmy.chyeh@gmail.com>
2020-08-05 13:52:00 +0800

    0_dcm2bids.sh: add sanity checks and multiple minor tweaks

--- 0_dcm2bids.sh
+++ 0_dcm2bids.sh
@@ -10,79 +10,71 @@
 ##---START OF SCRIPT----------------------------------------------------------------------------------------------------##
 ##########################################################################################################################
 
-Usage() {
-    cat<<EOF
+Usage(){
+
+cat<<EOF
     
-    dicom to bids  v1.0
+  dMRI processing pipeline v1.0
 
-    0_dcm2bids - DWI data preperation for the following processing
+    0_dcm2bids: Convert DICOM DWI data to BIDS format used in the subsequent processing scripts.
+                (The script assumes that the input directory contains *only* DWIs and anatomical DICOM data.)
 
-    Usage: 0_dcm2bids -d <dcmDir> -b <BIDSDir>
+    Usage: 0_dcm2bids -d <dcm_in> -b <bids_out>
 
 EOF
     exit
 }
 
-SubjName=
-dcmDir=
-BIDSDir=
-
-while getopts "hs:d:b:v" OPTION
-do
-    case $OPTION in
-    h)  
-        Usage
-        ;; 
+while getopts ":d:b:h:" OPTION ; do
+  case $OPTION in
     d)
-        dcmDir=$OPTARG
-        ;;
+      dcm_in=$OPTARG;;
     b)
-        BIDSDir=$OPTARG
-        ;;
-    v)
-        verbose=1
-        ;;
+      bids_out=$OPTARG;;
+    h)  
+      Usage;; 
     ?)
-        Usage
-        ;;
-    esac
+      Usage;;
+  esac
 done
 
-if [ "${dcmDir}" == "" ] || [ "${BIDSDir}" == "" ]; then
-    Usage
+
+# sanity checks
+if [ "${dcm_in}" == "" ] || [ "${bids_out}" == "" ]; then
+  Usage
+fi
+if [ ! -d "${dcm_in}" ] ; then
+  echo "[ERROR] Input DICOM directory does not exist."
+  exit
 fi
 
-mkdir -p ${BIDSDir}/0_BIDS/anat
-mkdir -p ${BIDSDir}/0_BIDS/dwi
 
-cd ${dcmDir}
+# convert dicom to json
+mkdir -p ${bids_out}/0_BIDS/anat ${bids_out}/0_BIDS/dwi
+cd ${dcm_in}
 
 all_folder=$(ls -d *)
-
 for folder in ${all_folder}; do
-	cd ${dcmDir}
 	dw_scheme=$(mrinfo ${folder} -property dw_scheme)
-	PD=$(mrinfo ${folder} -property PhaseEncodingDirection)
-	if [[ ! -z ${dw_scheme} ]]; then
+	pe_dir=$(mrinfo ${folder} -property PhaseEncodingDirection)
 
-		case "${PD}" in			
+	if [[ ! -z ${dw_scheme} ]]; then
+		case "${pe_dir}" in			
 			"j")
-				PED=PA
-				;;
+				PED=PA;;
 			"j-") 
-				PED=AP
-				;;
+				PED=AP;;
 			"i") 
-				PED=RL
-				;;
+				PED=RL;;
 			"i-") 
-				PED=LR
-				;;
-			esac
+				PED=LR;;
+    esac
 		mrconvert ${folder} dwi_${PED}.nii.gz -json_export dwi_${PED}.json -export_grad_fsl dwi_${PED}.bvec dwi_${PED}.bval
-		/bin/mv dwi_${PED}.* ${BIDSDir}/0_BIDS/dwi
+		mv dwi_${PED}.* ${bids_out}/0_BIDS/dwi
 	else
 		mrconvert ${folder} T1w.nii.gz -json_export T1w.json
-		/bin/mv T1w.* ${BIDSDir}/0_BIDS/anat
+		mv T1w.* ${bids_out}/0_BIDS/anat
 	fi
+
 done
+
