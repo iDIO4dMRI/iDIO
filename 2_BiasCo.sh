fac4d0d5f02b9d85098f2a3a52b9f713d4e0b34a
Yi-Chia Kung <orangekic@gmail.com>
2021-09-27 08:37:42 +0000

    Replace 2_BiasCo.sh

--- 2_BiasCo.sh
+++ 2_BiasCo.sh
@@ -7,6 +7,10 @@
 ##
 ## Edit: 2020/04/07, Tsen, Options
 ## Edit: 2020/04/20, Tsen, Options
+## Edit: 2021/02/18, Heather, (1) python, (2) dwicat, (3) replace dwi_select_vol with mrinfo and bvalue threshold
+## Edit: 2021/05/18, Heather, (1) bug fixed
+## Edit: 2021/07/28, Heather,  skip the denoise if the recon matrix is interpolated
+## Edit: 2021/09/23, Heather, output with noise map -> for QC purpose
 ##########################################################################################################################
 
 
@@ -21,13 +25,13 @@ Usage(){
 		 1_DWIprep is needed before processing this script.
 		 2_BiasCo will be created
 
-Usage:	2_BiasCo -[options] 
+Usage:	2_BiasCo -[options]
 
 System will automatically detect all folders in directory if no input arguments supplied
 
 Options:
-	-p 	Input directory; [default = pwd directory]	
-
+	-p 	Input directory(and output dir); [default = pwd directory]
+	-t  Input Bzero threshold; [default = 10]; 
 EOF
 exit 1
 }
@@ -39,17 +43,21 @@ arg=-1
 
 # Setup default variables
 OriDir=$(pwd)
+Bzerothr=10
 
 # Parse options
-while getopts "hp:" optionName; 
+while getopts "hp:t:" optionName;
 do
 	#echo "-$optionName is present [$OPTARG]"
 	case $optionName in
-	h)  
+	h)
 		Usage;;
 	p)
 		OriDir=$OPTARG;;
-	\?) 
+	t)
+		Bzerothr=$OPTARG
+		;;
+	\?)
 		exit 42;;
 	*)
 	  echo "Unrecognised option $1" 1>&2
@@ -58,7 +66,7 @@ do
 	esac
 done
 
-# The text file for phase encoding direction labeling: 0 0 0 0 = AP PA LR RL
+# The text file for phase encoding direction labeling: 0 0 0 0 = PA AP RL LR
 Phase_index=($(cat $OriDir/1_DWIprep/Index_PE.txt))
 Topup=$((${Phase_index[0]} + ${Phase_index[1]} + ${Phase_index[2]} + ${Phase_index[3]}))
 
@@ -111,6 +119,24 @@ else
 	File1=`ls *${direction[0]}.nii.gz`
 fi
 
+# Check recon matrix interpolation
+json_file=(`find ${OriDir}/0_BIDS_NIFTI -maxdepth 1 -name "*dwi*.json*"`)
+
+while read line; do
+	tmp=(${line})
+	case ${tmp[0]} in
+		'"AcquisitionMatrixPE":')
+			d=${tmp[1]}
+			AcquisitionMatrixPE=${d:0:${#d}-1}				
+		;;
+		'"ReconMatrixPE":')
+			d=${tmp[1]}
+			ReconMatrixPE=${d:0:${#d}-1}				
+		;;
+	esac
+done < $json_file
+
+
 case $Topup in
 	1 )
 		echo Input DWIs contain only one PE direction
@@ -120,9 +146,14 @@ case $Topup in
 		cp $(echo ${File1%.*.*}).bvec ${OriDir}/2_BiasCo
 
 		cd $OriDir/2_BiasCo
-		
-		dwidenoise $(echo ${File1%.*.*}).nii.gz Temp-denoise.nii.gz
-		mrdegibbs Temp-denoise.nii.gz $(echo ${File1%.*.*})-denoise-deGibbs.nii.gz #Keep the data format output from mrdegibbs
+		if [[ "$AcquisitionMatrixPE" == "$ReconMatrixPE" ]]; then
+			dwidenoise $(echo ${File1%.*.*}).nii.gz $(echo ${File1%.*.*})-denoise.nii.gz -noise $(echo ${File1%.*.*})-noise.nii.gz
+			mrdegibbs $(echo ${File1%.*.*})-denoise.nii.gz $(echo ${File1%.*.*})-denoise-deGibbs.nii.gz #Keep the data format output from mrdegibbs
+			# rm -f ./Temp-denoise.nii.gz
+		else
+			echo "interpolated Recon Matrix was found, skip denoise step"
+			mrdegibbs $(echo ${File1%.*.*}).nii.gz $(echo ${File1%.*.*})-deGibbs.nii.gz
+		fi
 		;;
 	3 )
 		echo Input DWIs contain more than one PE directions
@@ -130,35 +161,41 @@ case $Topup in
 		File2=`ls *${direction[1]}.nii.gz`
 		fslmerge -a $(echo ${File1%.*.*})${direction[1]} $File1 $File2
 
-		mv $(echo ${File1%.*.*})${direction[1]}.nii.gz $OriDir/2_BiasCo	
+		mv $(echo ${File1%.*.*})${direction[1]}.nii.gz $OriDir/2_BiasCo
 
 		bval1=$(cat $(echo ${File1%.*.*}).bval)
 		bval2=$(cat $(echo ${File2%.*.*}).bval)
 		echo $bval1 $bval2 > $OriDir/2_BiasCo/$(echo ${File1%.*.*})${direction[1]}.bval
-		
+
 		paste -d " " ${File1%.*.*}.bvec ${File2%.*.*}.bvec > $OriDir/2_BiasCo/$(echo ${File1%.*.*})${direction[1]}.bvec
-        
+
 		cd $OriDir/2_BiasCo
-		
-		dwidenoise $(echo ${File1%.*.*})${direction[1]}.nii.gz Temp-denoise.nii.gz
-		mrdegibbs Temp-denoise.nii.gz $(echo ${File1%.*.*})${direction[1]}-denoise-deGibbs.nii.gz #Keep the data format output from mrdegibbs
+		if [[ "$AcquisitionMatrixPE" == "$ReconMatrixPE" ]]; then
+			dwidenoise $(echo ${File1%.*.*})${direction[1]}.nii.gz $(echo ${File1%.*.*})-denoise.nii.gz -noise $(echo ${File1%.*.*})-noise.nii.gz
+			mrdegibbs $(echo ${File1%.*.*})-denoise.nii.gz $(echo ${File1%.*.*})${direction[1]}-denoise-deGibbs.nii.gz #Keep the data format output from mrdegibbs
+			# rm -f ./Temp-denoise.nii.gz
+		else
+			echo "interpolated Recon Matrix was found, skip denoise step"
+			mrdegibbs $(echo ${File1%.*.*})${direction[1]}.nii.gz $(echo ${File1%.*.*})${direction[1]}-deGibbs.nii.gz
+		fi
 		;;
 esac
 
 cd ${OriDir}/2_BiasCo
-rm -f ./Temp-denoise.nii.gz
-File_denoise=$(ls *-denoise-deGibbs.nii.gz | sed 's/.nii.gz//g')
+
+File_degibbs=$(ls *-deGibbs.nii.gz | sed 's/.nii.gz//g')
 File_bval=$(ls *.bval)
+File_bvec=$(ls *.bvec)
 
-select_dwi_vols ${File_denoise}.nii.gz $File_bval temp 0 > b0_report.txt
-B0num=$(echo $(cat b0_report.txt) | awk -F "--vols=" '/--vols=/{print $2}' | sed 's/,/ /g' | awk '{print NF}')
+#
+B0num=$(mrinfo ${File_degibbs}.nii.gz -fslgrad ${File_bvec} ${File_bval}  -shell_sizes -config BZeroThreshold ${Bzerothr}|awk '{print $1}')
 
+B0index=$(mrinfo ${File_degibbs}.nii.gz -fslgrad ${File_bvec} ${File_bval}  -shell_indices -config BZeroThreshold ${Bzerothr}|awk {'print $1'})
+
+#B0num > 3 -> do drift correction
 if [[ "${B0num}" -gt "3"  ]]; then
-	echo "Calling Matlab for Drifting Correction"
-	CMD="correct_signal_drift_v2('${File_denoise}.nii.gz', '${File_bval}', 0, 'multilinear', '${File_denoise}-DriftCo.nii.gz')"
-	matlab -nodisplay -r "${CMD}; quit"
+	echo "Calling python script for Drifting Correction"
+	python3 ${HOGIO}/python/driftco.py ${OriDir}/2_BiasCo/${File_degibbs}.nii.gz ${B0index} ${OriDir}/2_BiasCo/${File_degibbs}-DriftCo.nii.gz
 else
 	echo "Not enough number of b0 (null scans), drifting correction skipped"
 fi
-
-
