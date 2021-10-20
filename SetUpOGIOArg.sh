37ad5631ea9e5f26c4acdb080528155b17bf5f30
Heather <chihchin.heather@gmail.com>
2021-10-20 13:06:26 +0800

    add with S8_QC

--- SetUpOGIOArg.sh
+++ SetUpOGIOArg.sh
@@ -11,8 +11,8 @@
 # Edit as needed for your specific setup.
 # The defaults should work with most installations.
 
-# Set the process you need. [default=1.2.3.4.5.6.7]
-Step=1.2.3.4.5.6.7
+# Set the process you need. [default=1.2.3.4.5.6.7.8]
+Step=1.2.3.4.5.6.7.8
 # 1: 1_DWIprep
 # 2: 2_BiasCo
 # 3: 3_EddyCo
@@ -20,23 +20,24 @@ Step=1.2.3.4.5.6.7
 # 5: 5_DTIFIT
 # 6: 6_CSDpreproc
 # 7: 7_NetworkProc
+# 8: run_iDIOQC
 
 # 3_EddyCo: Using CUDA to speed up. NVIDIA GPU with CUDA version (8.0/9.1/10.2) is available to use this option. [true=1 / false=0]
 # cuda_ver=$(nvcc --version | grep release | cut -d ' ' -f 5 | sed 's/,//g') for CUDA version check
-cuda=1
+cuda=0
 
 # 3_EddyCo: Slice-to-vol motion correction.
 #           This option is only implemented for the CUDA version(cuda=1). [true=1 / false=0]
 stv=0
 
-# 3_EddyCo: Resize dwi image intp isotropic voxels (mm) by given value [true=1 / false=0]
+# 3_EddyCo: Resize dwi image intp isotropic voxels (mm) by given value [false=0]
 # apply for 3_EddyCo  [defult = 0 ]
 rsimg=0
 
-# 2_BiasCo, 3_EddyCo, 5_DTIFIT, 6_CSDpreproc: Bzero threshold. [default=10]
+# 2_BiasCo, 3_EddyCo, 5_DTIFIT, 6_CSDpreproc, 8_QC: Bzero threshold. [default=10]
 bzero=10
 
-# 4_T1preproc, 7_NetworkProc: Set input Atlas directory. [default=${HOGIO}/share]
+# 4_T1preproc, 7_NetworkProc, 8_QC: Set input Atlas directory. [default=${HOGIO}/share]
 AtlasDir=${HOGIO}/share
 
 # 7_NetworkProc: Set track select number. [default=10M]
